d8bbae9e528b60bfdc59390c9c7387305eedff6f
volkan <16579273+mevolkan@users.noreply.github.com>
2022-12-22 11:21:46 +0300

    wordpress hooks

--- form.php
+++ form.php
@@ -1,13 +1,30 @@
+<?php
 
-<!DOCTYPE html>
-<html>
-<body>
+/**
+ * Template Name: Warranty Template
+ */
+get_header(); ?>
+<?php header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Credentials: true");
+?>
+
+<?php
+if (has_post_thumbnail($post->ID)) :
+    $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail');
+?>
+    <div class="inner-banner">
+        <img src="<?php echo $image[0]; ?>" alt="" />
+    </div>
+<?php endif; ?>
+<?php while (have_posts()) : the_post(); ?>
+    <?php the_content(); ?>
+<?php endwhile; // end of the loop. 
+?>
 
 <form action="upload.php" method="post" enctype="multipart/form-data">
-  Select image to upload:
-  <input type="file" name="fileToUpload" id="fileToUpload">
-  <input type="submit" value="Upload Image" name="submit">
+      Select image to upload:
+      <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
 </form>
-
-</body>
-</html>
