d9e98c50fca494fa4e1b904904bb24cf1d62f318
volkan <16579273+mevolkan@users.noreply.github.com>
2023-02-16 15:08:11 +0300

    file handler needs work

--- scripts/file.js
+++ scripts/file.js
@@ -1,16 +1,14 @@
 // Upload file
-var resumePath;
+var fileData = []
 
-function uploadFiles (files,id) {
+function uploadFiles(files, id) {
     // var files = document.getElementById("resume").files;
-    console.log(files)
+    //console.log(files)
     if (files.length > 0) {
 
         var formData = new FormData();
         formData.append("file", files[0]);
-
         var xhttp = new XMLHttpRequest();
-     
         xhttp.open("POST", "/skunk/wp-content/plugins/job/ajax.php", true);
         xhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
@@ -19,22 +17,35 @@ function uploadFiles (files,id) {
                 response = JSON.parse(response);
 
                 // console.log(response.status);
-                console.log(response.url);
+                //console.log(response.url);
 
                 if (response.status == 1) {
                     alert("Upload successfully.");
-                    console.log(files[0].name)
+                    //console.log(files[0].name)
                 } else {
                     alert("File not uploaded.");
                 }
                 var cvUrl = document.getElementById(id);
                 cvUrl.textContent += `${response.url}`;
-                cvUrl.href += `./wp-content/plugins/job/`+`${response.url}`;
-                console.log(cvUrl);
+                cvUrl.href += `../wp-content/plugins/job/` + `${response.url}`;
+                //console.log(cvUrl.href);
+
+                const node = document.createElement("a");
+                const textnode = document.createTextNode(files[0].name);
+                node.appendChild(textnode);
+                node.href = `./wp-content/plugins/job/` + `${response.url}`;
+                node.title = files[0].name
+                document.getElementById(id).appendChild(node);
+                fileData.push({ [id]: node.href });
+
+                //console.log(fileData);
+
+
             }
         };
-
         xhttp.send(formData);
+        console.log(fileData);
+
 
     } else {
         alert("Please select a file");
@@ -46,48 +57,49 @@ function uploadFiles (files,id) {
 //senď form data
 
 function sendFormData() {
-
+    // console.log(fileData);
     const form = document.getElementById('careersform');
     const formData = new FormData(form);
-    const output = document.getElementById('output');
 
-    for (const [key, value] of formData) {
-        output.textContent += `${key}: ${value}\n`;
-    }
-    // console.log(formData);
+    // console.log(Object.fromEntries(formData));
     // formData= Object.fromEntries(formData)
-    applicationData = JSON.stringify(Object.fromEntries(formData))
-    console.log(applicationData);
+    if (fileData.length > 1) {
+        formData.set('resume', fileData[0].resume)
+        formData.set('cover_letter', fileData[1].cover_letter)
 
-
-    // // WARNING: For POST requests, body is set to null by browsers.
-    // var data = JSON.stringify({
-    //    "job_id": "18532",
-    //    "applicant_name": "John Doe",
-    //    "applicant_email": "john.doe@gmail.com",
-    //    "applicant_phone": "254712365478",
-    //    "applicant_mobile": "254123654789",
-    //    "years_experience": "3",
-    //    "salary_expected": "100000",
-    //    "availability": "2022-12-31",
-    //    "notice_period": "3 Months",
-    //    "cv": "",
-    //    "cover_letter": "",
-    //    "source_id": "1",
-    //    "referred_by": "sanergy_emp",
-    //    "highest_qualification": "2"
-    // });
-
-    var xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
-    xhr.addEventListener("readystatechange", function () {
-        if (this.readyState === 4) {
-            console.log(this.responseText);
+        for (const key in fileData) {
+            if (fileData.hasOwnProperty(key)) {
+                console.log(`${key}: ${fileData[key]}`);
+            }
         }
-    });
 
-    xhr.open("POST", "https://odoo.staging.saner.gy/apply-job");
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(applicationData);
+        var applicationData = JSON.stringify(Object.fromEntries(formData))
+
+        console.log(applicationData);
+
+
+        var url = "https://webhook.site/67d221e1-fa8e-48a1-95db-bcfad9bc9137";
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", url);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    console.log(xhr.response);
+                    console.log(xhr.responseText);
+                    alert('Success')
+                }
+            }
+        };
+
+        xhr.send(applicationData);
+
+    } else {
+        alert("Please select a file");
+    }
+
+
+
 
 }
