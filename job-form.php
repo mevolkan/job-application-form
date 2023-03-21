692a91bede6f3c9c475904524d28f9cd7e511f5d
volkan <16579273+mevolkan@users.noreply.github.com>
2023-03-21 15:07:42 +0300

    template with single job

--- job-form.php
+++ job-form.php
@@ -27,13 +27,139 @@ get_header(); ?>
             echo "Error: " . $error['message'];
         } else {
             // set the template variables
-            $title = 'Current Vacancies';
+            $title = 'Jobs List';
             $jobs = json_decode($response, true);
+
+            // Define the desired role ID
+            $role_id = 19111;
+
+            // Use array_filter to find the job object with the desired role ID
+            $singlejob = current(array_filter($jobs['jobs'], function ($singlejob) use ($role_id) {
+                return $singlejob['role_id'] === $role_id;
+            }));
+
+            // Check if the job object was found
+            if ($singlejob === null) {
+                echo 'Error: Job with role ID ' . $role_id . ' not found.';
+                die();
+            }
+
+            // Load the template file
+            require 'single_job.php';
+
+            // Set the title and jobs array for the template
+            $singlejobs = array($singlejob);
+
+            // Render the template
+            echo $template;
+
+        ?>
+            <form id="careersform">
+                <input name="job_id" id="job_id" type="number" hidden />
+                <div class='wpb_row vc_row-fluid vc_row inner_row  vc_row-o-equal-height vc_row-flex'>
+                    <div class="row_col_wrap_12 col span_12 dark left">
+                        <div class="vc_col-sm-6 wpb_column column_container vc_column_container col padding-4-percent inherit_tablet inherit_phone">
+                            <div class="vc_column-inner">
+                                <label for="applicant_name">Applicant Name</label>
+                                <input name="applicant_name" type="text" id="applcant_name" placeholder="Name" />
+                            </div>
+                        </div>
+                        <div class="vc_col-sm-6 wpb_column column_container vc_column_container col padding-4-percent inherit_tablet inherit_phone">
+                            <div class="vc_column-inner">
+                                <label for="applicant_email">Applicant Email</label>
+                                <input name="applicant_email" type="email" id="applcant_email" placeholder="Email" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row_col_wrap_12 col span_12 dark left">
+                        <div class="vc_col-sm-6 wpb_column column_container vc_column_container col padding-4-percent inherit_tablet inherit_phone">
+                            <div class="vc_column-inner">
+                                <label for="applicant_phone">Applicant Phone Number</label>
+                                <input name="applicant_phone" type="tel" placeholder="Phone Number" />
+                            </div>
+                        </div>
+                        <div class="vc_col-sm-6 wpb_column column_container vc_column_container col padding-4-percent inherit_tablet inherit_phone">
+                            <div class="vc_column-inner">
+                                <label for="applicant_mobile">Applicant Mobile Number</label>
+                                <input name="applicant_mobile" type="tel" placeholder="Mobile Number" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row_col_wrap_12 col span_12 dark left">
+                        <div class="vc_col-sm-6 wpb_column column_container vc_column_container col padding-4-percent inherit_tablet inherit_phone">
+                            <div class="vc_column-inner">
+                                <label for="years_experience">Years of Experience</label>
+                                <input type="number" name="years_experience" placeholder="X" />
+                            </div>
+                        </div>
+                        <div class="vc_col-sm-6 wpb_column column_container vc_column_container col padding-4-percent inherit_tablet inherit_phone">
+                            <div class="vc_column-inner">
+                                <label for="salary_expected">Salary Expected</label>
+                                <input type="number" name="salary_expected" placeholder="Salary Expected" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row_col_wrap_12 col span_12 dark left">
+                        <div class="vc_col-sm-6 wpb_column column_container vc_column_container col padding-4-percent inherit_tablet inherit_phone">
+                            <div class="vc_column-inner">
+                                <label for="availability">Availability</label>
+                                <input type="date" name="availability" placeholder="Date" />
+                            </div>
+                        </div>
+                        <div class="vc_col-sm-6 wpb_column column_container vc_column_container col padding-4-percent inherit_tablet inherit_phone">
+                            <div class="vc_column-inner">
+                                <label for="notice_period">Notice period</label>
+                                <input type="text" name="notice_period" placeholder="Notice period" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row_col_wrap_12 col span_12 dark left">
+                        <div class="vc_col-sm-6 wpb_column column_container vc_column_container col padding-4-percent inherit_tablet inherit_phone">
+                            <div class="vc_column-inner">
+                                <label for="resume">
+                                    CV</label>
+                                <input type="file" name="resume" id="resume" oninput="uploadFiles(this.files, this.id)" />
+                            </div>
+                        </div>
+                        <div class="vc_col-sm-6 wpb_column column_container vc_column_container col padding-4-percent inherit_tablet inherit_phone">
+                            <div class="vc_column-inner">
+                                <label for="cover_letter">
+                                    Cover Letter</label>
+                                <input type="file" name="cover_letter" id="cover_letter" oninput="uploadFiles(this.files, id)" />
+                            </div>
+                        </div>
+                    </div>
+                    <label></label>
+                    <input type="number" name="source_id" hidden value="1" />
+
+                    <label></label>
+                    <input type="text" name="referred_by" value="sanergy_emp" hidden />
+                    <br>
+                    <label>Highest Qualification</label>
+                    <select name="highest_qualification">
+                        <option value="8">Certificate</option>
+                        <option value="9">Diploma</option>
+                        <option value="7">Graduate</option>
+                        <option value="2">Bachelor Degree</option>
+                        <option value="3">Master Degree</option>
+                        <option value="4">Doctoral Degree</option>
+                    </select>
+
+                    <br>
+                    <a id="fileoutput" href=""></a>
+                    <br>
+                    <input type="button" id="btn_uploadfile" value="Submit Application" onclick="sendFormData();" />
+                </div>
+            </form>
+
+        <?php
 
             // render the template
             ob_start();
             include 'jobs_template.php';
             $content = ob_get_clean();
+
+
 
             // output the response
             echo $content;
@@ -43,107 +169,9 @@ get_header(); ?>
 
 
 
-        <form id="careersform">
-            <input name="job_id" id="job_id" type="number" hidden />
-            <div class='wpb_row vc_row-fluid vc_row inner_row  vc_row-o-equal-height vc_row-flex'>
-                <div class="row_col_wrap_12 col span_12 dark left">
-                    <div class="vc_col-sm-6 wpb_column column_container vc_column_container col padding-4-percent inherit_tablet inherit_phone">
-                        <div class="vc_column-inner">
-                            <label for="applicant_name">Applicant Name</label>
-                            <input name="applicant_name" type="text" id="applcant_name" placeholder="Name" />
-                        </div>
-                    </div>
-                    <div class="vc_col-sm-6 wpb_column column_container vc_column_container col padding-4-percent inherit_tablet inherit_phone">
-                        <div class="vc_column-inner">
-                            <label for="applicant_email">Applicant Email</label>
-                            <input name="applicant_email" type="email" id="applcant_email" placeholder="Email" />
-                        </div>
-                    </div>
-                </div>
-                <div class="row_col_wrap_12 col span_12 dark left">
-                    <div class="vc_col-sm-6 wpb_column column_container vc_column_container col padding-4-percent inherit_tablet inherit_phone">
-                        <div class="vc_column-inner">
-                            <label for="applicant_phone">Applicant Phone Number</label>
-                            <input name="applicant_phone" type="tel" placeholder="Phone Number" />
-                        </div>
-                    </div>
-                    <div class="vc_col-sm-6 wpb_column column_container vc_column_container col padding-4-percent inherit_tablet inherit_phone">
-                        <div class="vc_column-inner">
-                            <label for="applicant_mobile">Applicant Mobile Number</label>
-                            <input name="applicant_mobile" type="tel" placeholder="Mobile Number" />
-                        </div>
-                    </div>
-                </div>
-                <div class="row_col_wrap_12 col span_12 dark left">
-                    <div class="vc_col-sm-6 wpb_column column_container vc_column_container col padding-4-percent inherit_tablet inherit_phone">
-                        <div class="vc_column-inner">
-                            <label for="years_experience">Years of Experience</label>
-                            <input type="number" name="years_experience" placeholder="X" />
-                        </div>
-                    </div>
-                    <div class="vc_col-sm-6 wpb_column column_container vc_column_container col padding-4-percent inherit_tablet inherit_phone">
-                        <div class="vc_column-inner">
-                            <label for="salary_expected">Salary Expected</label>
-                            <input type="number" name="salary_expected" placeholder="Salary Expected" />
-                        </div>
-                    </div>
-                </div>
-                <div class="row_col_wrap_12 col span_12 dark left">
-                    <div class="vc_col-sm-6 wpb_column column_container vc_column_container col padding-4-percent inherit_tablet inherit_phone">
-                        <div class="vc_column-inner">
-                            <label for="availability">Availability</label>
-                            <input type="date" name="availability" placeholder="Date" />
-                        </div>
-                    </div>
-                    <div class="vc_col-sm-6 wpb_column column_container vc_column_container col padding-4-percent inherit_tablet inherit_phone">
-                        <div class="vc_column-inner">
-                            <label for="notice_period">Notice period</label>
-                            <input type="text" name="notice_period" placeholder="Notice period" />
-                        </div>
-                    </div>
-                </div>
-                <div class="row_col_wrap_12 col span_12 dark left">
-                    <div class="vc_col-sm-6 wpb_column column_container vc_column_container col padding-4-percent inherit_tablet inherit_phone">
-                        <div class="vc_column-inner">
-                            <label for="resume">
-                                CV</label>
-                            <input type="file" name="resume" id="resume" oninput="uploadFiles(this.files, this.id)" />
-                        </div>
-                    </div>
-                    <div class="vc_col-sm-6 wpb_column column_container vc_column_container col padding-4-percent inherit_tablet inherit_phone">
-                        <div class="vc_column-inner">
-                            <label for="cover_letter">
-                                Cover Letter</label>
-                            <input type="file" name="cover_letter" id="cover_letter" oninput="uploadFiles(this.files, id)" />
-                        </div>
-                    </div>
-                </div>
-                <label></label>
-                <input type="number" name="source_id" hidden value="1" />
 
-                <label></label>
-                <input type="text" name="referred_by" value="sanergy_emp" hidden />
-                <br>
-                <label>Highest Qualification</label>
-                <select name="highest_qualification">
-                    <option value="8">Certificate</option>
-                    <option value="9">Diploma</option>
-                    <option value="7">Graduate</option>
-                    <option value="2">Bachelor Degree</option>
-                    <option value="3">Master Degree</option>
-                    <option value="4">Doctoral Degree</option>
-                </select>
-
-                <br>
-                <a id="fileoutput" href=""></a>
-                <br>
-                <input type="button" id="btn_uploadfile" value="Submit Application" onclick="sendFormData();" />
-            </div>
-        </form>
     </div>
 
 </div>
-
-<div id="output"></div>
 <?php
 get_footer(); ?>
---
+++ single_job.php
@@ -0,0 +1,12 @@
+<?php foreach ($jobs as $job) : ?>
+    <h1><?php echo $job['role_name']; ?></h1>
+    <p><strong>Location:</strong> <?php echo $job['job_location']; ?></p>
+    <p><strong>Application Deadline:</strong> <?php echo $job['application_deadline']; ?></p>
+    <p><strong>Start Date:</strong> <?php echo $job['start_date']; ?></p>
+    <h2>About the Role</h2>
+    <?php echo $job['about_role']; ?>
+    <h2>Duties and Responsibilities</h2>
+    <?php echo $job['duties_and_responsibilities']; ?>
+    <h2>Qualifications</h2>
+    <?php echo $job['qualifications']; ?>
+<?php endforeach; ?>
