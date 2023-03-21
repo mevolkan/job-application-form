350ce26ac524ebb563b8e71acdd6c134b7c4778b
volkan <16579273+mevolkan@users.noreply.github.com>
2023-03-21 15:42:34 +0300

    testing template

--- single_job.php
+++ single_job.php
@@ -1,5 +1,8 @@
 <?php foreach ($filtered_jobs as $job) : ?>
     <h1><?php echo $job['role_name']; ?></h1>
+    <?php if ($job['role_name'] != null) : ?>
+        <h1> <?php echo $job['role_name']; ?></h1>
+    <?php endif; ?>
     <p><strong>Location:</strong> <?php echo $job['job_location']; ?></p>
     <p><strong>Application Deadline:</strong> <?php echo $job['application_deadline']; ?></p>
     <p><strong>Start Date:</strong> <?php echo $job['start_date']; ?></p>
@@ -7,9 +10,13 @@
     <?php echo $job['about_role']; ?>
     <h2>Duties and Responsibilities</h2>
     <?php echo $job['duties_and_responsibilities']; ?>
-    <h2>Qualifications</h2>
+    <?php if ($job['qualifications'] != null) : ?>
+        <h2>Qualifications</h2>
+        <p> <?php echo $job['qualifications']; ?></p>
+    <?php endif; ?>
+    
     <?php echo $job['qualifications']; ?>
-    <?php if ($job['start_date'] != null): ?>
-                <p><strong>Start Date:</strong> <?php echo $job['start_date']; ?></p>
-            <?php endif; ?>
+    <?php if ($job['start_date'] != null) : ?>
+        <p><strong>Start Date:</strong> <?php echo $job['start_date']; ?></p>
+    <?php endif; ?>
 <?php endforeach; ?>
