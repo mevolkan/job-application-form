43712e77427c331b52146cdca629c6d8bdc9f2f8
volkan <16579273+mevolkan@users.noreply.github.com>
2023-03-22 00:07:13 +0300

    form style

--- single_job.php
+++ single_job.php
@@ -1,20 +1,44 @@
-<?php foreach ($filtered_jobs as $job) : ?>
-    <?php if ($job['role_name'] != null) : ?>
-        <h1> <?php echo $job['role_name']; ?></h1>
-    <?php endif; ?>
-    <p><strong>Location:</strong> <?php echo $job['job_location']; ?></p>
-    <p><strong>Application Deadline:</strong> <?php echo $job['application_deadline']; ?></p>
-    <p><strong>Start Date:</strong> <?php echo $job['start_date']; ?></p>
-    <h2>About the Role</h2>
-    <?php echo $job['about_role']; ?>
-    <h2>Duties and Responsibilities</h2>
-    <?php echo $job['duties_and_responsibilities']; ?>
-    <?php if ($job['qualifications'] != null) : ?>
-        <h2>Qualifications</h2>
-        <p> <?php echo $job['qualifications']; ?></p>
-    <?php endif; ?>
-    <?php echo $job['qualifications']; ?>
-    <?php if ($job['start_date'] != null) : ?>
-        <p><strong>Start Date:</strong> <?php echo $job['start_date']; ?></p>
-    <?php endif; ?>
-<?php endforeach; ?>
+<div class="job-container ">
+    <?php foreach ($filtered_jobs as $job) : ?>
+        <?php if ($job['role_name'] != null) : ?>
+            <h1> <?php echo $job['role_name']; ?></h1>
+        <?php endif; ?>
+        <p>
+            <strong>
+                <?php if ($job['job_location'] != null) : ?>
+                    <?php echo $job['job_location']; ?>
+                <?php endif; ?>
+            </strong>
+        </p>
+        <p>
+            <strong>
+                <?php if ($job['application_deadline'] != null) : ?>
+                    <strong>Application Deadline:</strong> <?php echo $job['application_deadline']; ?>
+                <?php endif; ?>
+            </strong>
+        </p>
+        <p>
+            <strong>
+                <?php if ($job['start_date'] != null) : ?>
+                    <strong>Start Date:</strong><?php echo $job['start_date']; ?>
+                <?php endif; ?>
+            </strong>
+        </p>
+        <?php if ($job['about_role'] != null) : ?>
+            <h2>About the Role</h2>
+            <p> <?php echo $job['about_role']; ?></p>
+        <?php endif; ?>
+        <?php if ($job['duties_and_responsibilities'] != null) : ?>
+            <h2>Duties and Responsibilities</h2>
+            <p> <?php echo $job['duties_and_responsibilities']; ?></p>
+        <?php endif; ?>
+        <?php if ($job['qualifications'] != null) : ?>
+            <h2>Qualifications</h2>
+            <p> <?php echo $job['qualifications']; ?></p>
+        <?php endif; ?>
+        <?php echo $job['qualifications']; ?>
+        <?php if ($job['start_date'] != null) : ?>
+            <p><strong>Start Date:</strong> <?php echo $job['start_date']; ?></p>
+        <?php endif; ?>
+    <?php endforeach; ?>
+</div>
